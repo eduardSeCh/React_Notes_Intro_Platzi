f05fc75d5ea2b3d041d72ebf6344df0d5204bbce
eduard <eduardgsech@gmail.com>
2023-09-30 08:16:40 -0600

    modifie eslint to standard

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,8 +1,10 @@
+import { BsFillFileXFill } from 'react-icons/bs'
 import './css/TodoItem.css'
 
 function TodoItem ({ text, completed, onComplete, onDelete }) {
   return (
     <li className='TodoItem'>
+      {/* <CompleteIcon /> */}
       <span
         className={` Icon Icon-check ${completed && 'Icon-complete--active'}`}
         onClick={onComplete}
@@ -10,11 +12,12 @@ function TodoItem ({ text, completed, onComplete, onDelete }) {
         {completed ? '✅' : '☑️'}
       </span>
       <p className={`TodoItem-p ${completed && 'TodoItem-p--complete'}`}>{text}</p>
+      {/* <DeleteIcon /> */}
       <span
         className='Icon Icon-delete'
         onClick={onDelete}
       >
-        😢
+        <BsFillFileXFill />
       </span>
     </li>
   )
